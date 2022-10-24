3c95421a7d098292b4bbe2b0588565f29b6c583f
jijilak <95184520+jijilak@users.noreply.github.com>
2022-10-24 04:13:38 +0300

    Labs Part II

---
+++ src/ITMO/Laboratory2/Friend.java
@@ -0,0 +1,7 @@
+package ITMO.Laboratory2;
+
+public class Friend {
+    public static void main(String[] args){
+
+    }
+}
--- src/ITMO/Laboratory2/Main.java
+++ src/ITMO/Laboratory2/Main.java
@@ -3,21 +3,26 @@ package ITMO.Laboratory2;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
+        Calculator cator = new Calculator();
         System.out.println("Введите первую цифру");
         int a = sc.nextInt();
         System.out.println("Введите вторую цифру");
         int b = sc.nextInt();
-        Calculator cator = new Calculator();
+
         System.out.println("Сумма");
-        System.out.println(cator.sum (a, b));
+        double sum1 = cator.sum(a, b);
+        System.out.println(sum1);
         System.out.println("Деление");
-        System.out.println(cator.division (a, b));
+        double division1 = cator.division(a, b);
+        System.out.println(division1);
         System.out.println("Умножение");
-        System.out.println(cator.multiplication (a, b));
+        double multiplication1 = cator.multiplication(a, b);
+        System.out.println(multiplication1);
         System.out.println("Вычитание");
-        System.out.println(cator.subtraction (a, b));
+        double subtraction1 = cator.subtraction(a, b);
+        System.out.println(subtraction1);
     }
 }
