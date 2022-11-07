9e3dcff5aa498c530ae8d9db95effa9f5ef5e0a6
jijilak <95184520+jijilak@users.noreply.github.com>
2022-11-07 03:19:23 +0300

    Labs Part II

--- src/ITMO/Laboratory3/Main.java
+++ src/ITMO/Laboratory3/Main.java
@@ -2,7 +2,7 @@ package ITMO.Laboratory3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Car");
+        System.out.println("==Car==");
         Car car1 = new Car("Volvo");
         Car car2 = new Car("black", "1865");
         Car car3 = new Car();
@@ -14,7 +14,7 @@ public class Main {
         System.out.println(car3.printCar());
 
 
-        System.out.println( "House");
+        System.out.println( "==House==");
         House house1 = new House();
         House house2 = new House();
         house1.setHouse(5, 1762, "Hermitage-Winter Palace");
@@ -26,5 +26,19 @@ public class Main {
         System.out.println(house1.getYearsOfConstruction());
         System.out.println("Beloselsky-Belozersky Palace (years from the date of construction)");
         System.out.println(house2.getYearsOfConstruction());
+
+
+        System.out.println("==Tree==");
+        int years = 7;
+        boolean isAlive = true;
+        String name = "Birch";
+
+        Tree tree1 = new Tree(7, "Birch");
+        Tree tree2 = new Tree(75, true,  "Oak");
+        Tree tree3 = new Tree();
+
+        System.out.println(tree1.toString());
+        System.out.println(tree2.toString());
+        System.out.println(tree3.toString());
     }
 }
--- src/ITMO/Laboratory3/Tree.java
+++ src/ITMO/Laboratory3/Tree.java
@@ -1,4 +1,24 @@
 package ITMO.Laboratory3;
 
 public class Tree {
+    private int years;
+    private boolean isAlive;
+    private String name;
+
+    Tree () {
+        System.out.println("Пустой конструктор без параметров сработал");
+    }
+    Tree (int years, boolean isAlive, String name){
+        this.years = years;
+        this.isAlive = isAlive;
+        this.name = name;
+
+        System.out.println("Параметры дерева: " + "\nВозраст = " + years + "\nЖивое ли = " + isAlive + "\nНазвание = " + name + "\n");
+    }
+    Tree (int years, String name){
+        this.years = years;
+        this.name = name;
+
+        System.out.println("Параметры дерева: " + "\nВозраст = " + years + "\nНазвание = " + name + "\n");
+    }
 }
