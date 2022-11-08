76d12c5c7205dcd5bf69c443814abfcf6fd5a0e3
jijilak <95184520+jijilak@users.noreply.github.com>
2022-11-09 01:54:05 +0300

    Labs Part II

--- src/ITMO/Laboratory4/Part1.java
+++ src/ITMO/Laboratory4/Part1.java
@@ -2,15 +2,19 @@ package ITMO.Laboratory4;
 
 public class Part1 {
     public static  void  main(String[] args){
-        P1Ex1();
+        //P1Ex1();
         //P1Ex2();
        // P1Ex3();
         //P1Ex4();
     }
     //P1Ex1
     public static  void  P1Ex1(String[] args){
-        for (int n = 1; n < 99; n += 2){
+        int n = 1;
+        n += 2;
+        while (n <= 99){
+
         System.out.println(n);
+
     }
     }
 }
