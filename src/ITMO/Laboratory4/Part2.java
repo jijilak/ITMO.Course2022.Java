e045dc8439c467f1bbc20927def9508369462f51
jijilak <95184520+jijilak@users.noreply.github.com>
2022-11-10 01:02:38 +0300

    Labs Part III

---
+++ src/ITMO/Laboratory4/Part2.java
@@ -0,0 +1,36 @@
+package ITMO.Laboratory4;
+
+public class Part2 {
+    public static void main(String[] args) {
+        P2Ex1();
+        P2Ex2();
+        P2Ex3();
+        P2Ex4();
+        P2Ex5();
+        P2Ex6();
+    }
+
+    private static void P2Ex1() {
+
+    }
+
+    private static void P2Ex2() {
+
+    }
+
+    private static void P2Ex3() {
+
+    }
+
+    private static void P2Ex4() {
+
+    }
+
+    private static void P2Ex5() {
+
+    }
+
+    private static void P2Ex6() {
+
+    }
+    }
