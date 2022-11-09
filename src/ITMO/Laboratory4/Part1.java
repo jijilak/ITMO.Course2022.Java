56e31d9a4cae33eafc53a61ad88881499e4780b5
jijilak <95184520+jijilak@users.noreply.github.com>
2022-11-10 00:58:46 +0300

    Labs Part III

--- src/ITMO/Laboratory4/Part1.java
+++ src/ITMO/Laboratory4/Part1.java
@@ -10,24 +10,8 @@ public class Part1 {
         P1Ex3();
         P1Ex4();
         P1Ex5();
-        //P1Ex6();
+        P1Ex6();
     }
-
-    private static boolean P1Ex5() {
-        int[] array = new int[]{1, -6, 1, 5, 7, 3};
-
-        if (array[0] == 1 || array[array.length - 1] == 1) {
-            System.out.println("array = " + Arrays.toString(array) + "\ntrue");
-            return true;
-        } else if (array[0] == 3 || array[array.length - 1] == 3) {
-            System.out.println("array = " + Arrays.toString(array) + "\ntrue");
-            return true;
-        } else {
-            System.out.println("array = " + Arrays.toString(array) + "\nfalse");
-            return false;
-        }
-    }
-
     private static void P1Ex1() {
         int i = 1;
         while (i <= 99) {
@@ -35,7 +19,6 @@ public class Part1 {
             i += 2;
         }
     }
-
     public static void P1Ex2() {
         System.out.println("\nЧисло, которое делится на 3: ");
         int a = 3;
@@ -56,7 +39,6 @@ public class Part1 {
             c += 15;
         }
     }
-
     private static void P1Ex3() {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите первое число: ");
@@ -66,12 +48,11 @@ public class Part1 {
         System.out.println("Введите третье число: ");
         int c = in.nextInt();
         if (a + b == c) {
-            System.out.println("Результат: true");
+            System.out.println("Результат: true, т.к. сумма равна третьему целому числу");
         } else {
-            System.out.println("Результат: сумма не равна третьему целому числу!");
+            System.out.println("Результат: false, т.к. сумма не равна третьему целому числу!");
         }
     }
-
     private static void P1Ex4() {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите первое число: ");
@@ -81,7 +62,10 @@ public class Part1 {
         System.out.println("Введите третье число: ");
         int c = in.nextInt();
         if (a < b && c > b) {
-            System.out.println("Результат: true");
+            System.out.println("Результат: true, т.к. второе число больше первого числа, а третье число больше второго числа");
+        }
+        else {
+            System.out.println("Результат: false, т.к. не удовлетворяет условию");
         }
     }
     /*private static boolean P1Ex5() {
@@ -93,4 +77,29 @@ public class Part1 {
             System.out.println("array = " + Arrays.toString(array) + "\nfalse");
         }
     }*/
+    private static boolean P1Ex5() {
+        int[] array = new int[]{1, -6, 1, 5, 7, 3};
+
+        if (array[0] == 1 || array[array.length - 1] == 1) {
+            System.out.println("array = " + Arrays.toString(array) + "\ntrue, массив содержит число 1 или 3 как первый или последний элемент массива целых чисел");
+            return true;
+        } else if (array[0] == 3 || array[array.length - 1] == 3) {
+            System.out.println("array = " + Arrays.toString(array) + "\ntrue, массив содержит число 1 или 3 как первый или последний элемент массива целых чисел");
+            return true;
+        } else {
+            System.out.println("array = " + Arrays.toString(array) + "\nfalse, массив не содержит заданных чисел");
+            return false;
+        }
+    }
+    private static boolean P1Ex6() {
+        int[] array = new int[]{1, -6, 1, 9, 8, 3, -5};
+
+        if (array[0] == 1 || array[0] == 3) {
+            System.out.println("array = " + Arrays.toString(array) + "\ntrue, массив содержит число 1 или 3");
+            return true;
+        } else {
+            System.out.println("array = " + Arrays.toString(array) + "\nfalse, массив не содержит заданные числа");
+            return false;
+        }
+    }
 }
