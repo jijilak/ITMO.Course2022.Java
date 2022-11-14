e2f8100bef9e02faf77f225e4b24241f0d33a1c6
jijilak <95184520+jijilak@users.noreply.github.com>
2022-11-14 15:14:10 +0300

    Labs Part III

---
+++ src/ITMO/Laboratory6/Autotruck.java
@@ -0,0 +1,11 @@
+package ITMO.Laboratory6;
+
+public class Autotruck extends Car {
+    public int numberOfWheels;
+    public int maxWeight;
+
+    public  void newWheels(int numberOfWheels){
+        this.numberOfWheels = numberOfWheels;
+
+    }
+}
