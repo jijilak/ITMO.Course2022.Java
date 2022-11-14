61e8ff9b647794b3fb195a00e47388a7aa2d7f0c
jijilak <95184520+jijilak@users.noreply.github.com>
2022-11-14 07:13:04 +0300

    Labs Part III

--- src/ITMO/Laboratory4/Part1.java
+++ src/ITMO/Laboratory4/Part1.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 
 public class Part1 {
     public static void main(String[] args) {
+        int[] arrayA = new int[]{1, -6, 1, 5, 7, 3};
+        int[] arrayB = new int[]{1, -6, 1, 9, 8, 3, -5};
         P1Ex1();
         P1Ex2();
         P1Ex3();
         P1Ex4();
-        P1Ex5();
-        P1Ex6();
+        P1Ex5(arrayA);
+        P1Ex6(arrayB);
     }
     private static void P1Ex1() {
         int i = 1;
@@ -22,19 +24,19 @@ public class Part1 {
     public static void P1Ex2() {
         System.out.println("\nЧисло, которое делится на 3: ");
         int a = 3;
-        while (a <= 100 && a != 0) {
+        while (a <= 100) {
             System.out.println(a + " ");
             a += 3;
         }
         System.out.println("\nЧисло, которое делится на 5: ");
         int b = 5;
-        while (b <= 100 && b != 0) {
+        while (b <= 100) {
             System.out.println(b + " ");
             b += 5;
         }
         System.out.println("Число, которое делится на 15: ");
         int c = 15;
-        while (c <= 100 && c != 0) {
+        while (c <= 100) {
             System.out.println(c + " ");
             c += 15;
         }
@@ -77,28 +79,24 @@ public class Part1 {
             System.out.println("array = " + Arrays.toString(array) + "\nfalse");
         }
     }*/
-    private static boolean P1Ex5() {
-        int[] array = new int[]{1, -6, 1, 5, 7, 3};
-
-        if (array[0] == 1 || array[array.length - 1] == 1) {
-            System.out.println("array = " + Arrays.toString(array) + "\ntrue, массив содержит число 1 или 3 как первый или последний элемент массива целых чисел");
+    private static boolean P1Ex5(int[] arrayA) {
+        if (arrayA[0] == 1 || arrayA[arrayA.length - 1] == 1) {
+            System.out.println("array = " + Arrays.toString(arrayA) + "\ntrue, массив содержит число 1 или 3 как первый или последний элемент массива целых чисел");
             return true;
-        } else if (array[0] == 3 || array[array.length - 1] == 3) {
-            System.out.println("array = " + Arrays.toString(array) + "\ntrue, массив содержит число 1 или 3 как первый или последний элемент массива целых чисел");
+        } else if (arrayA[0] == 3 || arrayA[arrayA.length - 1] == 3) {
+            System.out.println("array = " + Arrays.toString(arrayA) + "\ntrue, массив содержит число 1 или 3 как первый или последний элемент массива целых чисел");
             return true;
         } else {
-            System.out.println("array = " + Arrays.toString(array) + "\nfalse, массив не содержит заданных чисел");
+            System.out.println("array = " + Arrays.toString(arrayA) + "\nfalse, массив не содержит заданных чисел");
             return false;
         }
     }
-    private static boolean P1Ex6() {
-        int[] array = new int[]{1, -6, 1, 9, 8, 3, -5};
-
-        if (array[0] == 1 || array[0] == 3) {
-            System.out.println("array = " + Arrays.toString(array) + "\ntrue, массив содержит число 1 или 3");
+    private static boolean P1Ex6(int[] arrayB) {
+        if (arrayB[0] == 1 || arrayB[0] == 3) {
+            System.out.println("array = " + Arrays.toString(arrayB) + "\ntrue, массив содержит число 1 или 3");
             return true;
         } else {
-            System.out.println("array = " + Arrays.toString(array) + "\nfalse, массив не содержит заданные числа");
+            System.out.println("array = " + Arrays.toString(arrayB) + "\nfalse, массив не содержит заданные числа");
             return false;
         }
     }
--- src/ITMO/Laboratory5/Exersize1.java
+++ src/ITMO/Laboratory5/Exersize1.java
@@ -1,4 +1,41 @@
 package ITMO.Laboratory5;
 
 public class Exersize1 {
+    public static String getWord(String date){
+        String array[] = date.split(" ");
+        int l = 0;
+        String word = " ";
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length() > l) {
+                l = array[i].length();
+                word = array[i];
+            }
+        }
+        return word;
+    }
+    public static boolean isWordPaliandrom(String date){
+        StringBuilder str = new StringBuilder(date);
+        String strPaliandrom = str.reverse().toString();
+        return date.equalsIgnoreCase(strPaliandrom);
+    }
+    public static String replacementBjaka (String date) {
+        return date.toLowerCase().replace("бяку", "[вырезано цензурой]");
+    }
+    public static int numberOfOccurrences (String Occurrences, String str){
+        int a = 0;
+        for (int i = str.indexOf(Occurrences); i >= 0; i = str.indexOf(Occurrences, i + 1)) {
+            a++;
+        }
+        return a;
+    }
+public static String strInversion(String str) {
+        String [ ] strArray = str.split (" ");
+        StringBuilder strBuilder = new StringBuilder();
+    for (int i = 0; i < strArray.length; i++) {
+        String reversedStr = new StringBuilder(strArray[i]).reverse().toString();
+        strBuilder.append(reversedStr)
+                .append(" ");
+        }
+        return strBuilder.toString().trim();
+}
 }
